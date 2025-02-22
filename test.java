aa4ba02a5dcaf356abfb92807c00a4dc0854e017
Angeliki Nikolaou <atzy.niko@gmail.com>
2025-02-22 11:07:29 +0200

    first commit

---
+++ test.java
@@ -0,0 +1,13 @@
+/**
+ * This is a test
+ */
+
+public class test {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello Coding Factorians!!");
+
+        System.out.println("Branch test");
+    }
+}
